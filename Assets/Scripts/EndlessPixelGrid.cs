e7b5243618c7fb578b1172e4a72db0e9432f56d7
jai Kumar <jai.kumar06@htl-salzburg.ac.at>
2026-01-30 15:45:01 +0100

    Implement toggle logic for input line pixels

--- Assets/Scripts/EndlessPixelGrid.cs
+++ Assets/Scripts/EndlessPixelGrid.cs
@@ -3,42 +3,43 @@ using UnityEngine.UI;
 
 public class EndlessPixelGrid : MonoBehaviour
 {
-    [Header("UI References")]
-    public Image[] gridPixels;      // 70 Images (10x7)
-    public Image[] inputPixels;     // 7 Images
+    public Image[] gridPixels;
+    public Image[] inputPixels;
 
-    // ===== DATENMODELL =====
-    private bool[,] grid = new bool[10, 7];   // Raster
-    private bool[] inputLine = new bool[7];   // Eingabezeile
+    private bool[,] grid = new bool[10, 7];
+    private bool[] inputLine = new bool[7];
 
-    void Start()
+    void Start() => RenderAll();
+
+    void Update()
     {
-        RenderAll();
+        if (Input.GetKeyDown(KeyCode.W)) ToggleInput(6);
+        if (Input.GetKeyDown(KeyCode.A)) ToggleInput(5);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) ToggleInput(4);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) ToggleInput(3);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) ToggleInput(2);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) ToggleInput(1);
+        if (Input.GetKeyDown(KeyCode.S)) ToggleInput(0);
     }
 
-    void RenderAll()
+    void ToggleInput(int index)
     {
-        RenderGrid();
+        inputLine[index] = !inputLine[index];
         RenderInputLine();
     }
 
+    void RenderAll() { RenderGrid(); RenderInputLine(); }
+
     void RenderGrid()
     {
-        for (int row = 0; row < 10; row++)
-        {
-            for (int col = 0; col < 7; col++)
-            {
-                int index = row * 7 + col;
-                gridPixels[index].color = grid[row, col] ? Color.white : Color.black;
-            }
-        }
+        for (int r = 0; r < 10; r++)
+            for (int c = 0; c < 7; c++)
+                gridPixels[r * 7 + c].color = grid[r, c] ? Color.white : Color.black;
     }
 
     void RenderInputLine()
     {
         for (int i = 0; i < 7; i++)
-        {
             inputPixels[i].color = inputLine[i] ? Color.white : Color.black;
-        }
     }
 }
